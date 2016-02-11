688241b707a759a77e6f151753ad4244a0a62648
js223kz <js223kz@student.lnu.se>
2016-02-11 11:54:54 +0100

    Structured application into different files

--- test/student-api-test.js
+++ test/student-api-test.js
@@ -15,13 +15,14 @@ let chai = require('chai'),
 
 chai.use(chaiAsPromised);
 
-
-describe('Testing routes', function () {
-    let baseUrl = '/';
-        it('should return a 200 response', function (done) {
-            local.get(baseUrl)
-            .set('Accept', 'application/json')
-            .expect(200,done);
+describe('Open file', function(){
+    it('should open file and return three objects',function(done){
+        students().then(function(data) {
+            expect(data[0]).to.have.property("city");
+            expect(data[0]).to.have.property("github");
+        }, function (err) {
+                console.log(err);
         });
+        done();
+    });    
 });
-
