b662f00b4bec8b7ba8c3f773ab665e5d969db41e
Angamanga <ki222bj@student.lnu.se>
2016-02-26 15:26:56 +0100

    sending initial state

--- app/controller/websocket.js
+++ app/controller/websocket.js
@@ -1,88 +1,47 @@
-var WebSocketServer = require('ws').Server;
-
-const data = 
-[
-    {
-        _owner: 'Visualisering',
-        _commitUser:{
-            _userName:"sk222sw",
-            _city:"Kalmar",
-            _lat:"56.665954",
-            _long:"16.356824"
-        },
-        _timestamp: '2016-02-17T10:39:23Z',
-        _code:"Här kommer kod" 
+const WebSocketServer = require('ws').Server;
+//initial-state mock-up
+const initialState = JSON.stringify({
+    matrix: {
+        data: [{
+            id: 1,
+            user: "sonny",
+            commit: "if (hej) {drink.Coffe()}"
+        }, {
+            id: 2,
+            user: "Lord Howell",
+            commit: "if (morning) {goto Work}"
+        }]
     },
-   {
-        _owner: 'Visualisering',
-        _commitUser:{
-            _userName:"js223kz",
-            _city:"Falsterbo",
-            _lat:"55.397107",
-            _long:"12.841880"
-        },
-        _timestamp: '2016-02-17T15:39:23Z',
-        _code:"Här kommer lite mer kod" 
-
+    settings: {
+        time: null
     },
-    {
-        _owner: 'Visualisering',
-        _commitUser:{
-            _userName:"jc222fi",
-            _city:"Borås",
-            _lat:"57.725040",
-            _long:"12.939528"
-        },
-        _timestamp: '2016-02-18T10:39:23Z',
-        _code: "Här kommer ännu mer kod"
-
+    sphere: {
+        data: [
+            {lat: 56.878655, lng: 14.819965, time: 300},
+            {lat: 54.242091, lng: 14.727779, time: 1020},
+            {lat: 39.399872, lng: -8.224454, time: 824},
+            {lat: 0, lng: 180, time: 404},
+            {lat: 0, lng: 90, time: 5083},
+            {lat: 0, lng: -90, time: 1204},
+            {lat: 0, lng: 0, time: 10}
+        ]
     },
-     {
-        _owner: 'Visualisering',
-        _commitUser:{
-            _userName:"mw222rs",
-            _city:"Umeå",
-            _lat:"63.829481",
-            _long:"20.262560"
-        },
-        _timestamp: '2016-02-18T15:39:23Z',
-        _code: "Härkommer ännu ännu mer kod"
+    codeCrawl: {
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi officia adipisci numquam eos sit. Autem vitae accusamus corporis laudantium ipsa ad impedit, quis possimus omnis rem excepturi illo sunt reprehenderit!"
+    }
+});
 
-    },
-    {
-        _owner: 'Visualisering',
-        _commitUser:{
-            _userName:"rk222ev",
-            _city:"Växjö",
-            _lat:"56.884362",
-            _long:"14.805126"
-        },
-        _timestamp: '2016-02-17T22:39:23Z',
-        _code: "Här kommer mest kod"
-
-    }   
-]
-
+//websocketServer
 module.exports = {
     startWebsocketServer(){
-        var wss = new WebSocketServer({port:8000});
-        wss.on('connection',function connection(ws){
-            ws.on('message', function incoming(message) {
+        var wss = new WebSocketServer({port:5000});
+        wss.on('connection', (ws)=>{
+            ws.on('message', (message)=>{
                 console.log('received: %s', message);
             });
-            ws.send(data);
-
+            ws.send(initialState);
         });
    }
 }
 
 
-
-
- /*const data = '[{"lat": 56.878655, "lng": 14.819965, "time": 300},' +
-    '{"lat": 54.242091, "lng": 14.727779, "time": 1020},' +
-    '{"lat": 39.399872, "lng": -8.224454, "time": 824}, ' +
-    '{"lat": 0, "lng": 180, "time": 404},' +
-    '{"lat": 0, "lng": 90, "time": 5083},' +
-    '{"lat": 0, "lng": -90, "time": 1204}, ' +
-    '{"lat": 0, "lng": 0, "time": 10}]';*/
