b14d2cc267f076e00341975c9d19d0b464bd6b34
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-27 14:59:39 +0100

    Remove unused code

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,16 +1,6 @@
 const _ = require("lodash");
 
-// THIS CODE AINT WORKING - JUST A TEST
-
 module.exports = {
-  // addNewCommitData(newCommitsArray) {
-  //   const currentState = getState().commits.data;
-  //   const newState = _.sortBy(currentState.concat(newCommitsArray), ["author", "date"]).slice(-100);
-
-  //   return ({type: "ADD_NEW_COMMITS", data: newState});
-
-      
-  // },
   addLatestCommits(commits) {
     return {type: "ADD_COMMITS", commits};
   }
